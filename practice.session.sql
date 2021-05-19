f34106bb326e0fb2a3609520724027382f0db7f1
sashkaool <sasha.dubilo@gmail.com>
2021-05-19 23:23:24 +0300

    delete profile all

--- practice.session.sql
+++ practice.session.sql
@@ -1,4 +1,6 @@
 /*UPDATE Lovice.Clubs SET Clubs.Club_name = 'Чайхана Урюк', Clubs.Adress = 'ул. Вавилонян, д18', Clubs.Description = 'Пю чй и мне хорошо...' WHERE Clubs.Id = 8;*/
 /*UPDATE Lovice.Users SET Users.Level = 'user' WHERE Users.Log_in = 'alexkorsak';*/
 /*delete from Lovice.Clubs where Clubs.Club_name = "";*/
-select * from Lovice.Services;
+-- INSERT INTO Lovice.Archive SELECT * FROM Lovice.Users WHERE Users.Log_in = 'olenevnik';
+-- SELECT * FROM Lovice.Archive
+-- delete from Lovice.Archive WHERE Id = 15;
